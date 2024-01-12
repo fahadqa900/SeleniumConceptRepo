5e8a5360eb650f3899cc31115189664e6bd2b8ca
unknown <fahadqa900@gmail.com>
2024-01-12 16:42:44 +0530

    Committed by fahad ali khan

--- src/com/metlife/frame/Demo1HDFC.java
+++ src/com/metlife/frame/Demo1HDFC.java
@@ -11,11 +11,15 @@ public class Demo1HDFC
     public static void main(String[] args)
     {
         WebDriver driver = new ChromeDriver();
+        //Maximize the browser window:
         driver.manage().window().maximize();
+        //Apply implicit wait of 20 mili seconds:
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
-
+        //Launching the browser:
         driver.get("https://netbanking.hdfcbank.com/netbanking/");
+        //Switch to Frame by using frame name:
         driver.switchTo().frame("login_page");
+        //Inspect the element on frame and enter text:
         driver.findElement(By.name("fldLoginUserId")).sendKeys("john@test.com");
 
     }
