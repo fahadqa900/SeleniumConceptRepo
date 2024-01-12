2e33e30b55acc446fc13470af91d3f00c31ad3ba
unknown <fahadqa900@gmail.com>
2024-01-12 15:55:57 +0530

    Committed by fahad ali khan

--- src/com/metlife/collections/DemoCollection2.java
+++ src/com/metlife/collections/DemoCollection2.java
@@ -7,16 +7,23 @@ public class DemoCollection2
 {
     public static void main(String[] args)
     {
+
         //HashMap<Integer,String> hash=new HashMap<Integer,String>();
         Map<Integer,String> hash=new HashMap<Integer,String>();
-
+        //Adding key values in HashMap collection:
         hash.put(101,"Demo1");
         hash.put(102,"Demo2");
         hash.put(103,"Demo3");
         hash.put(104,"Demo4");
+        //Replacing value of key 103 to new value:
         hash.put(103,"Replaced Value");
+        //Print new replaced value of key 103:
         System.out.println(hash.get(103));
-        System.out.println(hash.remove(101));
+        //Removing key 101 from collection:
+        hash.remove(101);
+        //Printing remaining key values of collection after removing key 101:
+        System.out.println(hash);
+        //Print size of collection:
         System.out.println(hash.size());
     }
 }
